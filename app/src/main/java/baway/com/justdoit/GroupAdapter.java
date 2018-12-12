e0923f9cac026882027519d7c030d98dd0d34958
chentaishan <chentaishan@autohome.com.cn>
2018-12-12 16:09:18 +0800

    去除 无关逻辑

--- app/src/main/java/baway/com/justdoit/GroupAdapter.java
+++ app/src/main/java/baway/com/justdoit/GroupAdapter.java
@@ -44,81 +44,20 @@ public class GroupAdapter extends ArrayAdapter<EMGroup> {
 	}
 
 	@Override
-	public int getViewTypeCount() {
-		return 4;
-	}
-
-	@Override
-	public int getItemViewType(int position) {
-		if (position == 0) {
-			return 0;
-		} else if (position == 1) {
-			return 1;
-		} else if (position == 2) {
-			return 2;
-		} else {
-			return 3;
-		}
-	}
-
-	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
-		if (getItemViewType(position) == 0) {
-			if (convertView == null) {
-				convertView = inflater.inflate(R.layout.em_search_bar_with_padding, parent, false);
-			}
-			final EditText query = (EditText) convertView.findViewById(R.id.query);
-			final ImageButton clearSearch = (ImageButton) convertView.findViewById(R.id.search_clear);
-			query.addTextChangedListener(new TextWatcher() {
-				public void onTextChanged(CharSequence s, int start, int before, int count) {
-					getFilter().filter(s);
-					if (s.length() > 0) {
-						clearSearch.setVisibility(View.VISIBLE);
-					} else {
-						clearSearch.setVisibility(View.INVISIBLE);
-					}
-				}
 
-				public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-				}
-
-				public void afterTextChanged(Editable s) {
-				}
-			});
-			clearSearch.setOnClickListener(new OnClickListener() {
-				@Override
-				public void onClick(View v) {
-					query.getText().clear();
-				}
-			});
-		} else if (getItemViewType(position) == 1) {
-			if (convertView == null) {
-				convertView = inflater.inflate(R.layout.em_row_add_group, parent, false);
-			}
-			((ImageView) convertView.findViewById(R.id.avatar)).setImageResource(R.drawable.em_create_group);
-			((TextView) convertView.findViewById(R.id.name)).setText(newGroup);
-		} else if (getItemViewType(position) == 2) {
-			if (convertView == null) {
-				convertView = inflater.inflate(R.layout.em_row_add_group, parent, false);
-			}
-			((ImageView) convertView.findViewById(R.id.avatar)).setImageResource(R.drawable.em_add_public_group);
-			((TextView) convertView.findViewById(R.id.name)).setText(addPublicGroup);
-			((TextView) convertView.findViewById(R.id.header)).setVisibility(View.VISIBLE);
-
-		} else {
 			if (convertView == null) {
 				convertView = inflater.inflate(R.layout.em_row_group, parent, false);
 			}
-			((TextView) convertView.findViewById(R.id.name)).setText(getItem(position - 3).getGroupName());
+			((TextView) convertView.findViewById(R.id.name)).setText(getItem(position ).getGroupName());
 
-		}
 
 		return convertView;
 	}
 
 	@Override
 	public int getCount() {
-		return super.getCount() + 3;
+		return super.getCount() ;
 	}
 
 }
